2dc591ab655393d2c00ef366df03323e0e6d1b6a
Gustavo Henrique de Castilho <gustavo.castilho@sulamerica.com.br>
2023-09-01 16:58:40 -0300

    :wrench: Ajuste de inclusão de configuração adicional

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
       baseUrl: 'https://plataforma-qa.dev-ads.audsat.io/app',
+      projectId: "dofksu",
   },
 })
